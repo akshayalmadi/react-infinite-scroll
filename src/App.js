150cd00a282d0f37075140c10864955a4b59ac90
Akshay Almadi <vikashkrchaubey@Akshays-MacBook-Pro.local>
2024-03-29 16:18:15 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useRef } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
+import InfiniteScroller from "./InfiniteScroller";
+import PostCard from "./PostCard";
+import "./styles.css";
+
+const fetchPosts = async ({ pageParam = 1 }) => {
+  const response = await fetch(
+    `https://picsum.photos/v2/list?page=${pageParam}&limit=5`
+  );
+  const results = await response.json();
+  return { results, nextPage: pageParam + 1, totalPages: 10 };
+};
+
+export default function App() {
+  const {
+    isLoading,
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage
+  } = useInfiniteQuery("posts", fetchPosts, {
+    getNextPageParam(lastPage) {
+      if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
+      return undefined;
+    }
+  });
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  return (
+    <div className="App">
+      <InfiniteScroller hasMore={hasNextPage} loadMore={fetchNextPage}>
+        {data.pages.map((page) =>
+          page.results.map((post) => <PostCard key={post.id} post={post} />)
+        )}
+      </InfiniteScroller>
+      {isFetchingNextPage && <h1>Fetching next page</h1>}
+    </div>
+  );
+}
